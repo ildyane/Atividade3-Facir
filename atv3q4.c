a781ce178d39064b282b128270f4046312c46ea0
ildyane <54899873+ildyane@users.noreply.github.com>
2019-09-26 14:06:46 -0300

    Add files via upload

---
+++ atv3q4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+  int valor;
+
+  printf("\n insira um número ");
+   scanf("%d]", & valor);
+
+   se (valor == 0);
+
+ printf ( " O valor %d e Zero. \ n " , valor);
+
+    se (valor> 0 );
+    {
+        printf ( " O valor % é positivo. \ n " , valor);
+    } mais {
+    }
+        printf ( " O valor % é negativo. \ n " , valor);
+    }
+
+   system ("pause");
