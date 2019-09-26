6dc98988b6e49b04b329500c5c33aeb422a22969
ildyane <54899873+ildyane@users.noreply.github.com>
2019-09-26 14:26:19 -0300

    Add files via upload

---
+++ atv3q10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+  float a, b;
+
+    printf ( " Insira dois numeros: \ n " );
+    scanf ( " % i  % i " , & a, & b);
+    se (a == b)
+    {
+        printf ( " Numeros iguais. \ n " );
+    } mais {
+    se (a> b)
+    {
+        printf ( " O primeiro e maior. \ n " );
+    } mais {
+        printf ( " O segundo e maior. \ n " );
+    }
+    }
+    sistema ( " pausa " );
+    retornar  0 ;
+}
