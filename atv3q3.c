38fc543c676c6daf4b5b2fa2265d97159b31a1a6
ildyane <54899873+ildyane@users.noreply.github.com>
2019-09-26 13:31:31 -0300

    Add files via upload

---
+++ atv3q3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+  float atual, max, min, med;
+
+ pintf ("\n ler a quantidade atual");
+ scanf ("%f", & atual);
+ printf ("\n ler a quantidade máxima");
+ scanf ("%f", & max);
+ printf ("\n ler a quantidade mínima");
+ scanf ("%f", & min);
+ printf ("\n ler a media");
+ scanf ("%f", med);
+   (med = max + min ) /2;
+if (atual>med);
+printf (" \n nao efetuar compra ");
+ } else {
+
+    printf(" efetuar compra ");
+
+    system("pause");
+
+}
