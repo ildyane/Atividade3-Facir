1a12f14605c3e4d65c37a9b01adf0856d0ae663b
ildyane <54899873+ildyane@users.noreply.github.com>
2019-09-27 14:49:50 -0300

    Add files via upload

---
+++ atv3q21.c
@@ -0,0 +1,17 @@
+
+# include  < stdio.h >
+# include  < stdlib.h >
+
+
+
+int  main ()
+{
+    int x;
+
+    for (x = 1 ; x <= 10 ; x ++)
+    {
+        printf ( " ( % i , 1 2 3 4 5 6 7 8 9 10) \ n " , x);
+    }
+    retornar  0 ;
+}
+
