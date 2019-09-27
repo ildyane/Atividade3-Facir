c10929704ca582f98373820d930c2988e3a5baa0
ildyane <54899873+ildyane@users.noreply.github.com>
2019-09-27 14:45:17 -0300

    Add files via upload

---
+++ atv3q17.c
@@ -0,0 +1,16 @@
+# include  <stdio.h>
+# include <stdlib.h>
+
+int  main () {
+
+int n, v;
+
+printf ( " informe o valor \ n " );
+scanf ( " % i " , & n);
+
+para (v = 1 ; v <n; v ++) {
+printf ( " % i " , v);
+}
+
+retornar  0 ;
+}
